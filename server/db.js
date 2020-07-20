03b07766b488c7bc7c7c2953688b31e26098b856
Golden Eagle <joowoon.kang@biola.edu>
2020-07-19 20:31:15 -0700

    finished the all crud operations

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool; // this will allow us to configure with postgresql
+
+const pool = new Pool({
+  user: "postgres",
+  password: "password",
+  host: "localhost",
+  database: "pernstack",
+  port: 5432,
+});
+
+module.exports = pool;
---
+++ server/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware using cors
+app.use(cors()); // this will allow local server from 5000 to 3000 to interact each other.
+
+app.use(express.json()); // allow us to access the req.body
+
+//Routes//
+
+//get all Todos
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo"); //the reason why it's not returning because it uses select
+
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id =$1", [id]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+//create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("Todo was updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo
+
+app.listen(5000, () => {
+  console.log("server is starting on port 5000");
+});
