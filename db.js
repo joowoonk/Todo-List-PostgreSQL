5fe0457d0f6f44d3737ccf832351696cbfab90c7
Golden Eagle <joowoon.kang@biola.edu>
2020-07-23 19:51:07 -0700

    .env is implentmented

--- db.js
+++ db.js
@@ -2,12 +2,30 @@
 
 const Pool = require("pg").Pool; // this will allow us to configure with postgresql
 
-const pool = new Pool({
-  user: "postgres",
-  password: "password",
-  host: "localhost",
-  database: "pernstack",
-  port: 5432,
-});
+require("dotenv").config();
+
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
+};
+
+// PG_USER = postgres
+// PG_PASSWORD = Kangonly34
+// PG_HOST = localhost
+// PG_PORT = 5432
+// PG_DATABASE = pernstack
+
+const pool = new Pool(devConfig);
 
 module.exports = pool;
+
+// {
+//   user: "postgres",
+//   password: "password",
+//   host: "localhost",
+//   database: "pernstack",
+//   port: 5432,
+// }
